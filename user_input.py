8de795c9189434d0ad811a8e82d27d1f0aa34d59
Dal1enst <joashotieno001@gmail.com>
2024-03-04 12:37:19 +0300

    My First Commit

---
+++ user_input.py
@@ -0,0 +1,10 @@
+#Defining the variables
+
+name= input('Kindly type in your name:')
+age= input('How old are you?')
+location= input('I wonder where you stay.')
+
+
+#Printing the output
+
+print('Salaam', name, '! Apparently you are', age, 'years old and stay in', location)
